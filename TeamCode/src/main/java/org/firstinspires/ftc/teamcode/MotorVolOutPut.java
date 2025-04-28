5a40ded19035cc4701b1346c817811f4e6b1bf60
Efxss <jrt1760@gmail.com>
2025-04-28 16:19:52 -0400

    Motor Velocity Testing

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorVolOutPut.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+
+@Config
+@Autonomous
+public class MotorVolOutPut extends OpMode {
+
+    private DcMotorEx frontLeft, frontRight, backLeft, backRight;
+
+    public static int P, I, D, F;
+
+    @Override public void init() {
+        frontLeft  = hardwareMap.get(DcMotorEx.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotorEx.class, "frontRight");
+        backLeft   = hardwareMap.get(DcMotorEx.class, "backLeft");
+        backRight  = hardwareMap.get(DcMotorEx.class, "backRight");
+
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+        telemetry.clear();
+    }
+
+    @Override public void loop() {
+        PIDFCoefficients FrontLeftPIDF = frontLeft.getPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER);
+        PIDFCoefficients FrontRightPIDF = frontRight.getPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        double FrontLeftP = FrontLeftPIDF.p, FrontLeftI = FrontLeftPIDF.i, FrontLeftD = FrontLeftPIDF.d, FrontLeftF = FrontLeftPIDF.f;
+        double FrontRightP = FrontRightPIDF.p, FrontRightI = FrontRightPIDF.i, FrontRightD = FrontRightPIDF.d, FrontRightF = FrontRightPIDF.f;
+
+        telemetry.addData("Front Left P", FrontLeftP);
+        telemetry.addData("Front Left I", FrontLeftI);
+        telemetry.addData("Front Left D", FrontLeftD);
+        telemetry.addData("Front Left F", FrontLeftF);
+        telemetry.addData("Front Right P", FrontRightP);
+        telemetry.addData("Front Right I", FrontRightI);
+        telemetry.addData("Front Right D", FrontRightD);
+        telemetry.addData("Front Right F", FrontRightF);
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorVolRun.java
@@ -0,0 +1,69 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Config
+@Autonomous
+public class MotorVolRun extends OpMode {
+
+    private DcMotorEx frontLeft, frontRight, backLeft, backRight;
+
+    public static int P, I, D, F;
+
+    @Override public void init() {
+        frontLeft  = hardwareMap.get(DcMotorEx.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotorEx.class, "frontRight");
+        backLeft   = hardwareMap.get(DcMotorEx.class, "backLeft");
+        backRight  = hardwareMap.get(DcMotorEx.class, "backRight");
+
+        ElapsedTime time = new ElapsedTime();
+
+        ResetEncoders();
+
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+        telemetry.clearAll();
+    }
+
+    @Override public void loop() {
+        frontLeft.setVelocity(1000);
+        frontRight.setVelocity(1000);
+        backLeft.setVelocity(1000);
+        backRight.setVelocity(1000);
+
+        telemetry.addData("Front Left Position", frontLeft.getCurrentPosition());
+        telemetry.addData("Front Right Position", frontRight.getCurrentPosition());
+        telemetry.addData("Back Left Position", backLeft.getCurrentPosition());
+        telemetry.addData("Back Right", backRight.getCurrentPosition());
+
+        telemetry.addData("Time", time);
+
+        if (time >= 10) {
+            requestOpModeStop();
+        }
+    }
+
+    @Override public void stop() {
+        frontLeft.setVelocity(0);
+        frontRight.setVelocity(0);
+        backLeft.setVelocity(0);
+        backRight.setVelocity(0);
+    }
+
+    private void ResetEncoders() {
+        frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+}
