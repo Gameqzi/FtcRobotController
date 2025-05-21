82776c0d1ecae99b3dad39a7f451f69a54dbef89
Efxss <jrt1760@gmail.com>
2025-05-21 19:19:51 -0400

    Added threading and latches to the code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDFTune.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDFTune.java
@@ -9,56 +9,85 @@ import com.qualcomm.hardware.sparkfun.SparkFunOTOS;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.RobotLog;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.threadopmode.TaskThread;
 import org.firstinspires.ftc.teamcode.threadopmode.ThreadOpMode;
+
+import java.util.concurrent.CountDownLatch;
 
 @Config
 @TeleOp
 public class PIDFTune extends ThreadOpMode {
 
     private DcMotorEx frontLeft, frontRight, backLeft, backRight;
+    private SparkFunOTOS SparkFun;
 
-    SparkFunOTOS SparkFun;
+    // PIDF coefficients
+    public static double P = 10, I = 3, D = 0, F = 8;
 
-    public static double P, I, D, F; //P = 10, I = 3, D = 0, F = 8
+    // Latches for synchronization
+    private final CountDownLatch startLatch = new CountDownLatch(1);
+    private final CountDownLatch stopLatch = new CountDownLatch(1);
 
-    @Override public void mainInit() {
+    // Flag to indicate running state
+    private volatile boolean running = true;
+
+    @Override
+    public void mainInit() {
         frontLeft  = hardwareMap.get(DcMotorEx.class, "frontLeft");
         frontRight = hardwareMap.get(DcMotorEx.class, "frontRight");
         backLeft   = hardwareMap.get(DcMotorEx.class, "backLeft");
         backRight  = hardwareMap.get(DcMotorEx.class, "backRight");
-        SparkFun = hardwareMap.get(SparkFunOTOS.class, "sensor_otos");
+        SparkFun   = hardwareMap.get(SparkFunOTOS.class, "sensor_otos");
 
+        // Motor directions (set as needed for your drivetrain)
         frontRight.setDirection(DcMotorEx.Direction.REVERSE);
         backRight.setDirection(DcMotorEx.Direction.FORWARD);
         frontLeft.setDirection(DcMotorEx.Direction.REVERSE);
         backLeft.setDirection(DcMotorEx.Direction.FORWARD);
 
-        ElapsedTime timer = new ElapsedTime();
-
-        P = 10;
-        I = 3;
-        D = 0;
-        F = 8;
-
         ResetEncoders();
+
+        // --- Register threads for each motor ---
+        registerThread(new TaskThread(() -> runMotorThread(frontLeft)));
+        registerThread(new TaskThread(() -> runMotorThread(frontRight)));
+        registerThread(new TaskThread(() -> runMotorThread(backLeft)));
+        registerThread(new TaskThread(() -> runMotorThread(backRight)));
 
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
         telemetry.clearAll();
         configureOtos();
+
+        // --- Release all threads to start at the same time ---
+        startLatch.countDown();
     }
 
-    boolean started = false;
-    @Override public void mainLoop() {
-        int FrontLeftPos = frontLeft.getCurrentPosition();
-        int FrontRightPos = frontRight.getCurrentPosition();
-        int BackLeftPos = backLeft.getCurrentPosition();
-        int BackRightPos = backRight.getCurrentPosition();
+    /**
+     * Main thread logic for each motor.
+     */
+    private void runMotorThread(DcMotorEx motor) {
+        try { startLatch.await(); } catch (InterruptedException e) { Thread.currentThread().interrupt(); }
 
+        while (running) {
+            SparkFunOTOS.Pose2D pos = SparkFun.getPosition();
+            if (pos.x < 48) {
+                motor.setVelocity(1000);
+            } else {
+                stopLatch.countDown();  // lets all threads stop their motors together
+            }
+            try { Thread.sleep(10); } catch (InterruptedException e) { Thread.currentThread().interrupt(); }
+        }
+
+        // Wait for stop signal from main thread for perfect sync
+        try { stopLatch.await(); } catch (InterruptedException e) { Thread.currentThread().interrupt(); }
+        motor.setVelocity(0);
+    }
+
+    @Override
+    public void mainLoop() {
+        // Update PIDF coefficients every loop (optional)
         frontRight.setVelocityPIDFCoefficients(P, I, D, F);
         backRight.setVelocityPIDFCoefficients(P, I, D, F);
         frontLeft.setVelocityPIDFCoefficients(P, I, D, F);
@@ -66,123 +95,52 @@ public class PIDFTune extends ThreadOpMode {
 
         SparkFunOTOS.Pose2D pos = SparkFun.getPosition();
 
-        if (!started) {
-            started = true;
-            setAllVelocity(1000);
-        } else if (pos.x > 48) {
-            setAllVelocity(0);
+        // --- Custom condition to stop all motors at the same instant ---
+        // You can replace this with any condition you want
+        if (pos.x > 48) {
+            running = false;        // tells threads to exit their loop
+            stopLatch.countDown();  // lets all threads stop their motors together
         }
 
-        RobotLog.dd("PIDFTune", "FrontLeft=%d  FrontRight=%d BackLeft=%d BackRight=%d", FrontLeftPos, FrontRightPos, BackLeftPos, BackRightPos);
-        RobotLog.dd("XH", "X=%f H=%f", pos.x, pos.h);
-
-        telemetry.addData("Front Left Position", FrontLeftPos);
-        telemetry.addData("Front Right Position", FrontRightPos);
-        telemetry.addData("Back Left Position", BackLeftPos);
-        telemetry.addData("Back Right Position", BackRightPos);
+        // Usual telemetry (optional)
+        telemetry.addData("Front Left Position", frontLeft.getCurrentPosition());
+        telemetry.addData("Front Right Position", frontRight.getCurrentPosition());
+        telemetry.addData("Back Left Position", backLeft.getCurrentPosition());
+        telemetry.addData("Back Right Position", backRight.getCurrentPosition());
         telemetry.addData("X", pos.x);
         telemetry.addData("Y", pos.y);
         telemetry.addData("H", pos.h);
+        telemetry.update();
     }
 
-    private void setAllVelocity(double velocity) {
-        backLeft.setVelocity(velocity);
-        backRight.setVelocity(velocity);
-        frontRight.setVelocity(velocity);
-        frontLeft.setVelocity(velocity);
+    private void ResetEncoders() {
+        frontLeft.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        frontLeft.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRight.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        frontRight.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeft.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        backLeft.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        backRight.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 
     @SuppressLint("DefaultLocale")
     private void configureOtos() {
-        telemetry.addLine("Configuring OTOS...");
-        telemetry.update();
-
-        // Set the desired units for linear and angular measurements. Can be either
-        // meters or inches for linear, and radians or degrees for angular. If not
-        // set, the default is inches and degrees. Note that this setting is not
-        // persisted in the sensor, so you need to set at the start of all your
-        // OpModes if using the non-default value.
-        // SparkFun.setLinearUnit(DistanceUnit.METER);
+        // ... (same as your existing sensor configuration logic) ...
         SparkFun.setLinearUnit(DistanceUnit.INCH);
-        // SparkFun.setAngularUnit(AnguleUnit.RADIANS);
         SparkFun.setAngularUnit(AngleUnit.DEGREES);
-
-        // Assuming you've mounted your sensor to a robot and it's not centered,
-        // you can specify the offset for the sensor relative to the center of the
-        // robot. The units default to inches and degrees, but if you want to use
-        // different units, specify them before setting the offset! Note that as of
-        // firmware version 1.0, these values will be lost after a power cycle, so
-        // you will need to set them each time you power up the sensor. For example, if
-        // the sensor is mounted 5 inches to the left (negative X) and 10 inches
-        // forward (positive Y) of the center of the robot, and mounted 90 degrees
-        // clockwise (negative rotation) from the robot's orientation, the offset
-        // would be {-5, 10, -90}. These can be any value, even the angle can be
-        // tweaked slightly to compensate for imperfect mounting (eg. 1.3 degrees).
-        //SparkFunOTOS.Pose2D offset = new SparkFunOTOS.Pose2D(0, 0, 0);
-        //SparkFun.setOffset(offset);
-
-        // Here we can set the linear and angular scalars, which can compensate for
-        // scaling issues with the sensor measurements. Note that as of firmware
-        // version 1.0, these values will be lost after a power cycle, so you will
-        // need to set them each time you power up the sensor. They can be any value
-        // from 0.872 to 1.127 in increments of 0.001 (0.1%). It is recommended to
-        // first set both scalars to 1.0, then calibrate the angular scalar, then
-        // the linear scalar. To calibrate the angular scalar, spin the robot by
-        // multiple rotations (eg. 10) to get a precise error, then set the scalar
-        // to the inverse of the error. Remember that the angle wraps from -180 to
-        // 180 degrees, so for example, if after 10 rotations counterclockwise
-        // (positive rotation), the sensor reports -15 degrees, the required scalar
-        // would be 3600/3585 = 1.004. To calibrate the linear scalar, move the
-        // robot a known distance and measure the error; do this multiple times at
-        // multiple speeds to get an average, then set the linear scalar to the
-        // inverse of the error. For example, if you move the robot 100 inches and
-        // the sensor reports 103 inches, set the linear scalar to 100/103 = 0.971
         SparkFun.setLinearScalar(1.0);
         SparkFun.setAngularScalar(1.0);
-
-        // The IMU on the OTOS includes a gyroscope and accelerometer, which could
-        // have an offset. Note that as of firmware version 1.0, the calibration
-        // will be lost after a power cycle; the OTOS performs a quick calibration
-        // when it powers up, but it is recommended to perform a more thorough
-        // calibration at the start of all your OpModes. Note that the sensor must
-        // be completely stationary and flat during calibration! When calling
-        // calibrateImu(), you can specify the number of samples to take and whether
-        // to wait until the calibration is complete. If no parameters are provided,
-        // it will take 255 samples and wait until done; each sample takes about
-        // 2.4ms, so about 612ms total
         SparkFun.calibrateImu();
-
-        // Reset the tracking algorithm - this resets the position to the origin,
-        // but can also be used to recover from some rare tracking errors
         SparkFun.resetTracking();
-
-        // After resetting the tracking, the OTOS will report that the robot is at
-        // the origin. If your robot does not start at the origin, or you have
-        // another source of location information (eg. vision odometry), you can set
-        // the OTOS location to match and it will continue to track from there.
         SparkFunOTOS.Pose2D currentPosition = new SparkFunOTOS.Pose2D(0, 0, 0);
         SparkFun.setPosition(currentPosition);
-
-        // Get the hardware and firmware version
         SparkFunOTOS.Version hwVersion = new SparkFunOTOS.Version();
         SparkFunOTOS.Version fwVersion = new SparkFunOTOS.Version();
         SparkFun.getVersionInfo(hwVersion, fwVersion);
-
-        /*telemetry.addLine("OTOS configured! Press start to get position data!");
-        telemetry.addLine();
-        telemetry.addLine(String.format("OTOS Hardware Version: v%d.%d", hwVersion.major, hwVersion.minor));
-        telemetry.addLine(String.format("OTOS Firmware Version: v%d.%d", fwVersion.major, fwVersion.minor));
-        telemetry.update();*/
-    }
-
-    private void ResetEncoders() {
-        frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        frontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        frontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        backLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        backRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
     }
 }
