dff915d0ad918ed84651b2cf6cf7c94dd3de78cd
efxss <jrt1760@gmail.com>
2025-06-13 17:48:43 -0400

    Added script to make sure building fully worked on linux

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BuildTest.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.threadopmode.ThreadOpMode;
+
+@TeleOp
+public class BuildTest extends ThreadOpMode {
+    @Override
+    public void mainInit() {
+
+    }
+
+    @Override
+    public void mainLoop() {
+        telemetry.addData("script uploaded", null);
+    }
+}
