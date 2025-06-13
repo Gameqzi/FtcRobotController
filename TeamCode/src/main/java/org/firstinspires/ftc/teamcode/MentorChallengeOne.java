15b36af24b37958742df37a698c08ec90083f4e2
efxss <jrt1760@gmail.com>
2025-06-13 19:08:18 -0400

    Added MentorChallengeOne

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MentorChallengeOne.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode;
+
+import org.firstinspires.ftc.teamcode.threadopmode.ThreadOpMode;
+
+public class MentorChallengeOne extends ThreadOpMode {
+    @Override
+    public void mainInit() {
+
+    }
+
+    @Override
+    public void mainLoop() {
+
+    }
+}
