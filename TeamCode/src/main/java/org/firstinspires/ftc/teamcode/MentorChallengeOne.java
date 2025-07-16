489b7abfcb77cc6cffedb6a0a944cf5bca84636a
efxss <jrt1760@gmail.com>
2025-07-16 18:23:13 -0400

    Test updates

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MentorChallengeOne.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MentorChallengeOne.java
@@ -150,13 +150,16 @@ public class MentorChallengeOne extends ThreadOpMode {
 
         telemetry.addData("X1:", X1);
         telemetry.addData("Y1:", Y1);
-        telemetry.addData("H1 (RAD):", "%.10f", H1);
+        telemetry.addData("H1", "%.10f", H1);
         telemetry.addLine();
 
         telemetry.addData("X2:", X2);
         telemetry.addData("Y2:", Y2);
-        telemetry.addData("H2 (RAD):", "%.10f", H2);
+        telemetry.addData("H2", "%.10f", H2);
         telemetry.addLine();
+
+        sleep(5000);
+        requestOpModeStop();
 
         telemetry.addLine("Mathing...");
         telemetry.addLine(" !Be Aware: Could divide by 0 or <1e-6, very unlikely though!"); // POTENTIAL CATASTROPHIC ERROR: DIVIDE BY 0 OR <1e-6!!! IDK: How to fix/If even issue
@@ -226,9 +229,11 @@ public class MentorChallengeOne extends ThreadOpMode {
                     telemetry.update();
 
                     if (targetError > 10) {
-                        robot.rotateRight(Math.max(Math.abs(targetError) / 1500, 0.05));
+                        //robot.rotateRight(Math.max(Math.abs(targetError) / 1500, 0.05));
+                        robot.rotateRight(0.1);
                     } else if (targetError < -10) {
-                        robot.rotateLeft(Math.max(Math.abs(targetError) / 1500, 0.05));
+                        //robot.rotateLeft(Math.max(Math.abs(targetError) / 1500, 0.05));
+                        robot.rotateLeft(0.1);
                     }
 
                     sleep(25);
@@ -285,85 +290,16 @@ public class MentorChallengeOne extends ThreadOpMode {
 //region SparkFunOTOS Config
     @SuppressLint("DefaultLocale")
     private void configureOtos() {
-        telemetry.addLine("Configuring OTOS...");
-        telemetry.update();
-
-        // Set the desired units for linear and angular measurements. Can be either
-        // meters or inches for linear, and radians or degrees for angular. If not
-        // set, the default is inches and degrees. Note that this setting is not
-        // persisted in the sensor, so you need to set at the start of all your
-        // OpModes if using the non-default value.
         // SparkFun.setLinearUnit(DistanceUnit.METER);
         robot.getImu().setLinearUnit(DistanceUnit.INCH);
         // SparkFun.setAngularUnit(AngleUnit.RADIANS);
         robot.getImu().setAngularUnit(AngleUnit.DEGREES);
-
-        // Assuming you've mounted your sensor to a robot and it's not centered,
-        // you can specify the offset for the sensor relative to the center of the
-        // robot. The units default to inches and degrees, but if you want to use
-        // different units, specify them before setting the offset! Note that as of
-        // firmware version 1.0, these values will be lost after a power cycle, so
-        // you will need to set them each time you power up the sensor. For example, if
-        // the sensor is mounted 5 inches to the left (negative X) and 10 inches
-        // forward (positive Y) of the center of the robot, and mounted 90 degrees
-        // clockwise (negative rotation) from the robot's orientation, the offset
-        // would be {-5, 10, -90}. These can be any value, even the angle can be
-        // tweaked slightly to compensate for imperfect mounting (eg. 1.3 degrees).
-        //SparkFunOTOS.Pose2D offset = new SparkFunOTOS.Pose2D(0, 0, 0);
-        //SparkFun.setOffset(offset);
-
-        // Here we can set the linear and angular scalars, which can compensate for
-        // scaling issues with the sensor measurements. Note that as of firmware
-        // version 1.0, these values will be lost after a power cycle, so you will
-        // need to set them each time you power up the sensor. They can be any value
-        // from 0.872 to 1.127 in increments of 0.001 (0.1%). It is recommended to
-        // first set both scalars to 1.0, then calibrate the angular scalar, then
-        // the linear scalar. To calibrate the angular scalar, spin the robot by
-        // multiple rotations (eg. 10) to get a precise error, then set the scalar
-        // to the inverse of the error. Remember that the angle wraps from -180 to
-        // 180 degrees, so for example, if after 10 rotations counterclockwise
-        // (positive rotation), the sensor reports -15 degrees, the required scalar
-        // would be 3600/3585 = 1.004. To calibrate the linear scalar, move the
-        // robot a known distance and measure the error; do this multiple times at
-        // multiple speeds to get an average, then set the linear scalar to the
-        // inverse of the error. For example, if you move the robot 100 inches and
-        // the sensor reports 103 inches, set the linear scalar to 100/103 = 0.971
         robot.getImu().setLinearScalar(1.0);
         robot.getImu().setAngularScalar(1.0);
-
-        // The IMU on the OTOS includes a gyroscope and accelerometer, which could
-        // have an offset. Note that as of firmware version 1.0, the calibration
-        // will be lost after a power cycle; the OTOS performs a quick calibration
-        // when it powers up, but it is recommended to perform a more thorough
-        // calibration at the start of all your OpModes. Note that the sensor must
-        // be completely stationary and flat during calibration! When calling
-        // calibrateImu(), you can specify the number of samples to take and whether
-        // to wait until the calibration is complete. If no parameters are provided,
-        // it will take 255 samples and wait until done; each sample takes about
-        // 2.4ms, so about 612ms total
         robot.getImu().calibrateImu();
-
-        // Reset the tracking algorithm - this resets the position to the origin,
-        // but can also be used to recover from some rare tracking errors
         robot.getImu().resetTracking();
-
-        // After resetting the tracking, the OTOS will report that the robot is at
-        // the origin. If your robot does not start at the origin, or you have
-        // another source of location information (eg. vision odometry), you can set
-        // the OTOS location to match and it will continue to track from there.
         SparkFunOTOS.Pose2D currentPosition = new SparkFunOTOS.Pose2D(0, 0, 0);
         robot.getImu().setPosition(currentPosition);
-
-        // Get the hardware and firmware version
-        SparkFunOTOS.Version hwVersion = new SparkFunOTOS.Version();
-        SparkFunOTOS.Version fwVersion = new SparkFunOTOS.Version();
-        robot.getImu().getVersionInfo(hwVersion, fwVersion);
-
-        telemetry.addLine("OTOS configured! Press start to get position data!");
-        telemetry.addLine();
-        telemetry.addLine(String.format("OTOS Hardware Version: v%d.%d", hwVersion.major, hwVersion.minor));
-        telemetry.addLine(String.format("OTOS Firmware Version: v%d.%d", fwVersion.major, fwVersion.minor));
-        telemetry.update();
     }
 //endregion
 
