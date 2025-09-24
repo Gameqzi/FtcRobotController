67dc21f699d8fd0a58a26c858f633a49674acd4e
Efxss <jrt1760@gmail.com>
2025-09-24 16:42:36 -0400

    Should be able to change power for each motor in panels

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OutTake.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OutTake.kt
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode
 
+import com.bylazar.configurables.annotations.Configurable
 import com.bylazar.telemetry.PanelsTelemetry
 import com.bylazar.telemetry.TelemetryManager
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
@@ -8,12 +9,17 @@ import com.qualcomm.robotcore.hardware.DcMotorEx
 import com.qualcomm.robotcore.hardware.DcMotorSimple
 import org.firstinspires.ftc.teamcode.Utils.sleep
 
+@Configurable
 @TeleOp
 class OutTake : OpMode() {
+    companion object {
+        @JvmField
+        var power1 = 0.toDouble()
+        var power2 = 0.toDouble()
+    }
     private var panels: TelemetryManager? = null
     private lateinit var motor1: DcMotorEx
     private lateinit var motor2: DcMotorEx
-    var power = 1.toDouble()
     var ar = arrayOf("p", "p", "g") // Test to make a array
 
     override fun init() {
@@ -24,16 +30,10 @@ class OutTake : OpMode() {
     }
 
     override fun loop() {
-        if (gamepad1.dpad_up) {
-            power + 0.1
-            sleep(1000)
-        } else if (gamepad1.dpad_down) {
-            power - 0.1
-            sleep(1000)
-        }
-        motor1.power = power
-        motor2.power = power
-        panels!!.addData("Power", power)
+        motor1.power = power1
+        motor2.power = power2
+        panels!!.addData("Power1", power1)
+        panels!!.addData("Power2", power2)
         panels!!.addData("Real Motor 1 Power", motor1.power)
         panels!!.addData("Real Motor 2 Power", motor2.power)
     }
