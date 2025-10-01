0915cdb331da32ee64d2c865f43d3548a9613ee5
efxss <jrt1760@gmail.com>
2025-10-01 18:40:34 -0400

    Made the system for saving and reading apriltags a lot better

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ReadTower.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ReadTower.kt
@@ -5,28 +5,48 @@ import com.bylazar.telemetry.TelemetryManager
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
 import java.io.File
-import java.io.Serializable
 
 @Autonomous
 class ReadTower : OpMode() {
     private var panels: TelemetryManager? = null
     private val fileName = "array.txt"
+    var order = arrayOf("", "", "") //Place holder at start of match will change when tag is detected
     override fun init() {
+        var currentOrder = getOrder()
         panels = PanelsTelemetry.telemetry
     }
 
     override fun loop() {
-        var order = read().toString()
-        panels?.addData("Order", order)
-        panels?.update()
+
     }
 
-    private fun read(): Serializable {
+    private fun read(): Int {
         val file = File(hardwareMap.appContext.filesDir, fileName)
-        return if (file.exists()) {
+        return (if (file.exists()) {
             file.readText()
         } else {
             0
+        }) as Int
+    }
+
+    fun getOrder() {
+        var tagID = read()
+        when (tagID) {
+            21 -> {
+                order[0] = "G"
+                order[1] = "P"
+                order[2] = "P"
+            }
+            22 -> {
+                order[0] = "P"
+                order[1] = "G"
+                order[2] = "P"
+            }
+            23 -> {
+                order[0] = "P"
+                order[1] = "P"
+                order[2] = "G"
+            }
         }
     }
 }
