47de31db9d7156ca871d3a920d00666d67e3bdeb
Efxss <135909007+Efxss@users.noreply.github.com>
2025-10-20 14:51:46 -0400

    Added a servo test script

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTest.kt
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode
+
+import com.bylazar.telemetry.PanelsTelemetry
+import com.bylazar.telemetry.TelemetryManager
+import com.qualcomm.robotcore.eventloop.opmode.OpMode
+import com.qualcomm.robotcore.hardware.Servo
+import java.lang.Thread.sleep
+import kotlin.concurrent.thread
+
+class ServoTest : OpMode() {
+    private var panels: TelemetryManager? = null
+    private lateinit var servo : Servo
+    override fun init() {
+        servo = hardwareMap.get(Servo::class.java, "Servo")
+        panels = PanelsTelemetry.telemetry
+    }
+
+    override fun loop() {
+        sleep(2000)
+        servo.position = 0.4
+        sleep(2000)
+        servo.position = 0.8
+        sleep(2000)
+        servo.position = 1.toDouble()
+        panels?.addData("position", servo.position)
+    }
+}
